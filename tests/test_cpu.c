b850c5d60ce075959d114d0d96c7da94e6406762
Joseph Scollo <jscollo@oswego.edu>
2026-04-06 10:12:18 -0400

    implemented and proved DEY

--- tests/test_cpu.c
+++ tests/test_cpu.c
@@ -3974,7 +3974,6 @@ void test_DEX_returns_two_cycles(void)
     mem[0x8000] = 0xCA;
 
     cpu_reset(&cpu, mem);
-    cpu.X = 0xFC;
     int cycle = cpu_step(&cpu, mem);
 
     TEST_ASSERT_EQUAL(2, cycle);
@@ -3995,6 +3994,125 @@ void test_DEX_wraps_around(void)
 
     TEST_ASSERT_EQUAL(0xFF, cpu.X);
 }
+
+/** DEX Tests *****************************************************************************************************/
+void test_DEY_decrements_y_register(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0x88;
+
+    cpu_reset(&cpu, mem);
+    cpu.Y = 0xFC;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0XFB, cpu.Y);
+}
+
+void test_DEY_sets_zero_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0x88;
+
+    cpu_reset(&cpu, mem);
+    cpu.Y = 0x01;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(1, cpu.Z);
+    TEST_ASSERT_EQUAL(0, cpu.N);
+}
+
+void test_DEY_sets_negative_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0x88;
+
+    cpu_reset(&cpu, mem);
+    cpu.Y = 0x00;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0, cpu.Z);
+    TEST_ASSERT_EQUAL(1, cpu.N);
+}
+
+void test_DEY_clears_zero_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0x88;
+
+    cpu_reset(&cpu, mem);
+    cpu.Z = 1;
+    cpu.Y = 0x02;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0, cpu.Z);
+    TEST_ASSERT_EQUAL(0, cpu.N);
+}
+
+void test_DEY_clears_negative_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0x88;
+
+    cpu_reset(&cpu, mem);
+    cpu.N = 1;
+    cpu.Y = 0x80;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0, cpu.Z);
+    TEST_ASSERT_EQUAL(0, cpu.N);
+}
+
+void test_DEY_returns_two_cycles(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0x88;
+
+    cpu_reset(&cpu, mem);
+    int cycle = cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(2, cycle);
+}
+
+void test_DEY_wraps_around(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0x88;
+
+    cpu_reset(&cpu, mem);
+    cpu.Y = 0x00;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0xFF, cpu.Y);
+}
+
 
 int main(void) {
 
@@ -4286,5 +4404,15 @@ int main(void) {
     RUN_TEST(test_DEX_clears_negative_flag);
     RUN_TEST(test_DEX_returns_two_cycles);
     RUN_TEST(test_DEX_wraps_around);
+
+    //DEY Tests
+    //Implied
+    RUN_TEST(test_DEY_decrements_y_register);
+    RUN_TEST(test_DEY_sets_zero_flag);
+    RUN_TEST(test_DEY_sets_negative_flag);
+    RUN_TEST(test_DEY_clears_zero_flag);
+    RUN_TEST(test_DEY_clears_negative_flag);
+    RUN_TEST(test_DEY_returns_two_cycles);
+    RUN_TEST(test_DEY_wraps_around);
     return UNITY_END();
 }
