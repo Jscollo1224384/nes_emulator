e6732f66ba5da2d31f867cb1cc00b02e8718d99b
Joseph Scollo <jscollo@oswego.edu>
2026-04-17 06:57:07 -0400

    implemented and proved AND indirect y

--- tests/test_cpu.c
+++ tests/test_cpu.c
@@ -5358,6 +5358,175 @@ void test_AND_indirect_x_returns_six_cycles(void)
     TEST_ASSERT_EQUAL(6, cycle);
 }
 
+/** AND Indirect X Tests ****************************************************************************************/
+void test_AND_indirect_y_ands_accumulator_with_indirect_y_offset_value(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b01000010; //0x42
+    mem[0x8002] = 0x31;
+    mem[0x8003] = 0x00;
+    mem[0x0000] = 0x00;
+    mem[0x0001] = 0x20;
+    mem[0x2005] = 0b01000000; //0x40
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu.Y = 0x05;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0b01000000, cpu.A);
+}
+
+void test_AND_indirect_y_sets_zero_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b01000010; //0x42
+    mem[0x8002] = 0x31;
+    mem[0x8003] = 0x00;
+    mem[0x0000] = 0x00;
+    mem[0x0001] = 0x20;
+    mem[0x2005] = 0b00000000; //0x00
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu.Y = 0x05;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(1, cpu.Z);
+    TEST_ASSERT_EQUAL(0, cpu.N);
+}
+
+void test_AND_indirect_y_sets_negative_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b10000001; //0x81
+    mem[0x8002] = 0x31;
+    mem[0x8003] = 0x00;
+    mem[0x0000] = 0x00;
+    mem[0x0001] = 0x20;
+    mem[0x2005] = 0b10000000; //0x80
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu.Y = 0x05;
+    cpu.N = 0;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0, cpu.Z);
+    TEST_ASSERT_EQUAL(1, cpu.N);
+}
+
+void test_AND_indirect_y_clears_zero_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b00000001; //0x01
+    mem[0x8002] = 0x31;
+    mem[0x8003] = 0x00;
+    mem[0x0000] = 0x00;
+    mem[0x0001] = 0x20;
+    mem[0x2005] = 0b00000001; //0x01
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu.Y = 0x05;
+    cpu.Z = 1;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0, cpu.Z);
+    TEST_ASSERT_EQUAL(0, cpu.N);
+}
+
+void test_AND_indirect_y_clears_negative_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b10000001; //0x81
+    mem[0x8002] = 0x31;
+    mem[0x8003] = 0x00;
+    mem[0x0000] = 0x00;
+    mem[0x0001] = 0x20;
+    mem[0x2005] = 0b00000001; //0x01
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu.Y = 0x05;
+    cpu.N = 1;
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0, cpu.Z);
+    TEST_ASSERT_EQUAL(0, cpu.N);
+}
+
+void test_AND_indirect_y_returns_five_cycles(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b01000010; //0x42
+    mem[0x8002] = 0x31;
+    mem[0x8003] = 0x00;
+    mem[0x0000] = 0x00;
+    mem[0x0001] = 0x20;
+    mem[0x2005] = 0b01000000; //0x40
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu.Y = 0x05;
+    int cycle = cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(5, cycle);
+}
+
+void test_AND_indirect_y_returns_six_cycles_on_page_cross(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b01000010; //0x42
+    mem[0x8002] = 0x31;
+    mem[0x8003] = 0x00;
+    mem[0x0000] = 0xFF;
+    mem[0x0001] = 0x20;
+    mem[0x2104] = 0b01000000; //0x40
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu.Y = 0x05;
+    int cycle = cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(6, cycle);
+}
+
 int main(void) {
 
     UNITY_BEGIN();
@@ -5743,5 +5912,14 @@ int main(void) {
     RUN_TEST(test_AND_indirect_x_clears_zero_flag);
     RUN_TEST(test_AND_indirect_x_clears_negative_flag);
     RUN_TEST(test_AND_indirect_x_returns_six_cycles);
+
+    //Indirect Y
+    RUN_TEST(test_AND_indirect_y_ands_accumulator_with_indirect_y_offset_value);
+    RUN_TEST(test_AND_indirect_y_sets_zero_flag);
+    RUN_TEST(test_AND_indirect_y_sets_negative_flag);
+    RUN_TEST(test_AND_indirect_y_clears_zero_flag);
+    RUN_TEST(test_AND_indirect_y_clears_negative_flag);
+    RUN_TEST(test_AND_indirect_y_returns_five_cycles);
+    RUN_TEST(test_AND_indirect_y_returns_six_cycles_on_page_cross);
     return UNITY_END();
 }
