45e2b9122a49a28e570c119d13057d589746c172
Joseph Scollo <38252783+Jscollo1224384@users.noreply.github.com>
2026-05-21 08:26:43 -0400

    implemented and proved EOR zero page (#49)

--- tests/test_cpu.c
+++ tests/test_cpu.c
@@ -7122,6 +7122,133 @@ void test_EOR_immediate_returns_two_cycles(void)
     TEST_ASSERT_EQUAL(2, cycle);
 }
 
+/** ORA Zero Page Tests *************************************************************************************************/
+void test_EOR_zero_page_performs_eor_operation(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b01000000; //0x40
+    mem[0x8002] = 0x45;
+    mem[0x8003] = 0x02;
+    mem[0x0002] = 0b00000010; //0x02
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(0b01000010, cpu.A);
+}
+
+void test_EOR_zero_page_sets_zero_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b01000010; //0x42
+    mem[0x8002] = 0x45;
+    mem[0x8003] = 0x02;
+    mem[0x0002] = 0b01000010; //0x42
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_BITS(FLAG_Z, 0b00000010, cpu.P);
+    TEST_ASSERT_BITS(FLAG_N, 0b00000000, cpu.P);
+}
+
+void test_EOR_zero_page_sets_negative_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b01000010; //0x42
+    mem[0x8002] = 0x45;
+    mem[0x8003] = 0x02;
+    mem[0x0002] = 0b11000010; //0xC2
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_BITS(FLAG_Z, 0b00000000, cpu.P);
+    TEST_ASSERT_BITS(FLAG_N, 0b10000000, cpu.P);
+}
+
+void test_EOR_zero_page_clears_zero_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b00000000; //0x00
+    mem[0x8002] = 0x45;
+    mem[0x8003] = 0x02;
+    mem[0x0002] = 0b00000001; //0x01
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    set_flag(&cpu, FLAG_Z);
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_BITS(FLAG_Z, 0b00000000, cpu.P);
+    TEST_ASSERT_BITS(FLAG_N, 0b00000000, cpu.P);
+}
+
+void test_EOR_zero_page_clears_negative_flag(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b10000000; //0x80
+    mem[0x8002] = 0x45;
+    mem[0x8003] = 0x02;
+    mem[0x0002] = 0b10000001; //0x81
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    set_flag(&cpu, FLAG_N);
+    cpu_step(&cpu, mem);
+
+    TEST_ASSERT_BITS(FLAG_Z, 0b00000000, cpu.P);
+    TEST_ASSERT_BITS(FLAG_N, 0b00000000, cpu.P);
+}
+
+void test_EOR_zero_page_returns_three_cycles(void)
+{
+    CPU cpu;
+    uint8_t mem[0x10000] = {0};
+
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x80;
+    mem[0x8000] = 0xA9;
+    mem[0x8001] = 0b01000000; //0x40
+    mem[0x8002] = 0x45;
+    mem[0x8003] = 0x02;
+    mem[0x0002] = 0b00000010; //0x02
+
+    cpu_reset(&cpu, mem);
+    cpu_step(&cpu, mem);
+    int cycle = cpu_step(&cpu, mem);
+
+    TEST_ASSERT_EQUAL(3, cycle);
+}
+
 int main(void) {
 
     UNITY_BEGIN();
@@ -7639,5 +7766,13 @@ int main(void) {
     RUN_TEST(test_EOR_immediate_clears_zero_flag);
     RUN_TEST(test_EOR_immediate_clears_negative_flag);
     RUN_TEST(test_EOR_immediate_returns_two_cycles);
+
+    //Zero-page
+    RUN_TEST(test_EOR_zero_page_performs_eor_operation);
+    RUN_TEST(test_EOR_zero_page_sets_zero_flag);
+    RUN_TEST(test_EOR_zero_page_sets_negative_flag);
+    RUN_TEST(test_EOR_zero_page_clears_zero_flag);
+    RUN_TEST(test_EOR_zero_page_clears_negative_flag);
+    RUN_TEST(test_EOR_zero_page_returns_three_cycles);
     return UNITY_END();
 }
