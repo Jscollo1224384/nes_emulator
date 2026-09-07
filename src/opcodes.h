781e5b018a1adbba64f39554996e4f8a5f389ad8
Joseph Scollo <jscollo@oswego.edu>
2026-09-07 10:34:13 -0400

    Added function and opcode

--- src/opcodes.h
+++ src/opcodes.h
@@ -87,6 +87,7 @@ int op_eor_immediate(CPU *cpu, uint8_t *mem);
 int op_eor_zeropage(CPU *cpu, uint8_t *mem);
 int op_eor_zeropage_x(CPU *cpu, uint8_t *mem);
 int op_eor_absolute(CPU *cpu, uint8_t *mem);
+int op_eor_absolute_x(CPU *cpu, uint8_t *mem);
 
 // Default handler for unimplemented opcodes
 int op_unimplemented(CPU *cpu, uint8_t *mem);
