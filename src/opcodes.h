e1167acebe3ed8fa11e27f1c0421bc08deb91906
Joseph Scollo <jscollo@oswego.edu>
2026-09-09 07:47:51 -0400

    implemnted and proved eor absolute y

--- src/opcodes.h
+++ src/opcodes.h
@@ -88,6 +88,7 @@ int op_eor_zeropage(CPU *cpu, uint8_t *mem);
 int op_eor_zeropage_x(CPU *cpu, uint8_t *mem);
 int op_eor_absolute(CPU *cpu, uint8_t *mem);
 int op_eor_absolute_x(CPU *cpu, uint8_t *mem);
+int op_eor_absolute_y(CPU *cpu, uint8_t *mem);
 
 // Default handler for unimplemented opcodes
 int op_unimplemented(CPU *cpu, uint8_t *mem);
